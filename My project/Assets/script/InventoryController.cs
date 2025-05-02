d97fc75a5e80e192e49a3c0b4960bd27b1b1050f
Axonaute <elliotyou2@outlook.fr>
2025-05-02 14:54:50 -0400

    wdqwdwdqqwddwdwdwdq

---
+++ My project/Assets/script/InventoryController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+   [SerializeField] ItemGrid selectedItemGrid;
+
+    private void Update()
+    {
+        if(selectedItemGrid == null) { return; }
+        Debug.Log(selectedItemGrid.GetTileGridPosition(Input.mousePosition));
+    }
+}
