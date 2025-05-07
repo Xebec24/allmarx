c51f7315ce31e6ef48f291b517f24eeb85606580
Axonaute <elliotyou2@outlook.fr>
2025-05-07 14:59:41 -0400

    dwqwdqdqdqddqw

--- My project/Assets/script/ItemGrid.cs
+++ My project/Assets/script/ItemGrid.cs
@@ -6,18 +6,21 @@ public class ItemGrid : MonoBehaviour
 {
     const float tileSizeWidth = 16;
     const float tileSizeHeight = 16;
-    InventoryItem[,] InventoryItemSlot;
+    InventoryItem[,] inventoryItemSlot;
     RectTransform rectTransform;
     [SerializeField] int gridSizeWidth = 20;
     [SerializeField] int gridSizeHeight = 10;
+    [SerializeField] GameObject inventoryItemPrefab;    
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         Init(gridSizeWidth, gridSizeHeight);
+        InventoryItem inventoryItem = Instantiate(inventoryItemPrefab).GetComponent<InventoryItem>();
+        PlaceItem(inventoryItem, 3, 2);
     }
     private void Init(int width, int height)
     {
-        InventoryItemSlot = new InventoryItem[width, height];
+        inventoryItemSlot = new InventoryItem[width, height];
         Vector2 size = new Vector2(width * tileSizeWidth, height * tileSizeHeight);
         rectTransform.sizeDelta = size;
     }
@@ -32,5 +35,16 @@ public class ItemGrid : MonoBehaviour
         tileGridPosition.y = (int)(positionOnTheGrid.y / tileSizeHeight);
 
         return tileGridPosition;
+    }
+    public void PlaceItem(InventoryItem inventoryItem, int posX, int posY)
+    {
+        RectTransform rectTransform = inventoryItem.GetComponent<RectTransform>();
+        rectTransform.SetParent(this.rectTransform);
+        inventoryItemSlot[posX, posY] = inventoryItem;
+        Vector2 position = new Vector2();
+        position.x = posX * gridSizeWidth + gridSizeWidth / 2;
+        position.y = posY * gridSizeHeight + gridSizeHeight / 2;
+        rectTransform.localPosition = position;
+
     }
 }
